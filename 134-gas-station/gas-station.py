d21d9b0c8d3d610125b8ef07abb1d66c0e57f55a
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-09 23:32:40 -0800

    Update gas-station.py

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -10,33 +10,3 @@ class Solution:
                 cur_tank = 0
 
         return ans if total_tank >=0 else -1
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # cur_tank,total_tank,start_pos = 0,0,0
-        # for i in range(len(gas)):
-        #     cur_tank+=gas[i]-cost[i]
-        #     total_tank+=gas[i]-cost[i]
-        #     if cur_tank<0:
-        #         start_pos = i+1
-        #         cur_tank = 0
-        # return start_pos if total_tank>=0 else -1
-
-        
