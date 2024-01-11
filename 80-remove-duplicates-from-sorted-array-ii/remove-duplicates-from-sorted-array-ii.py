81949143858866908ad778e79875502330e5fdd7
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-11 09:47:49 -0800

    Time: 50 ms (88.24%) | Memory: 17.4 MB (19.53%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -11,39 +11,6 @@ class Solution:
             else:
                 c=1
             p1+=1
-        return len(nums)
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # n=len(nums)
-        # p1=1
-        # c=1
-        # while p1<len(nums) :
-        #     if nums[p1]==nums[p1-1]:
-        #         c+=1
-        #         if c>2:
-        #             nums.pop(p1)
-        #             p1-=1
-        #     else:
-        #         c=1
-        #     p1+=1
         
         
 
