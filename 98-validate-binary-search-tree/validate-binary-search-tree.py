fd931eb5a713858226f682baa3eb96df937597d9
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-12 18:58:16 -0800

    Time: 92 ms (5.25%) | Memory: 19.2 MB (29.28%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -7,31 +7,74 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         def mn(root):
-            if root == None:
+            if not root:
                 return 2**32
             res = root.val
-            lres = mn(root.left)
-            rres = mn(root.right)
-            res = min(lres,res,rres)
+            l_res = mn(root.left)
+            r_res = mn(root.right)
+            res = min(res,l_res,r_res)
             return res
-        
         def mx(root):
-            if root == None:
+            if not root:
                 return -2**32
             res = root.val
-            lres = mx(root.left)
-            rres = mx(root.right)
-            res = max(lres,res,rres)
+            l_res = mx(root.left)
+            r_res = mx(root.right)
+            res = max(res,l_res,r_res)
             return res
-        
-        def isbst(root):
-            if root == None:
+        def is_bst(root):
+            if not root:
                 return True
-            if root.left != None and mx(root.left)>=root.val:
+            if root.left and mx(root.left)>=root.val:
                 return False
-            if root.right != None and mn(root.right)<=root.val:
+            if root.right and mn(root.right)<=root.val:
                 return False
-            if isbst(root.left) == False or isbst(root.right) == False:
+            if not (is_bst(root.left) and is_bst(root.right)):
                 return False
             return True
-        return isbst(root)
+        return is_bst(root) 
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def mn(root):
+        #     if root == None:
+        #         return 2**32
+        #     res = root.val
+        #     lres = mn(root.left)
+        #     rres = mn(root.right)
+        #     res = min(lres,res,rres)
+        #     return res
+        
+        # def mx(root):
+        #     if root == None:
+        #         return -2**32
+        #     res = root.val
+        #     lres = mx(root.left)
+        #     rres = mx(root.right)
+        #     res = max(lres,res,rres)
+        #     return res
+        
+        # def isbst(root):
+        #     if root == None:
+        #         return True
+        #     if root.left != None and mx(root.left)>=root.val:
+        #         return False
+        #     if root.right != None and mn(root.right)<=root.val:
+        #         return False
+        #     if not (isbst(root.left) and isbst(root.right)):
+        #         return False
+        #     return True
+        # return isbst(root)
