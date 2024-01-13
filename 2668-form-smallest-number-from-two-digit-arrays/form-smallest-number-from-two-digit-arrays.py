c3f85ec84e41eb3c64c2421a4c28d14ab5c9724a
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-12 23:53:32 -0800

    Time: 37 ms (77.10%) | Memory: 17.4 MB (16.28%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.py
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.py
@@ -1,9 +1,24 @@
 class Solution:
     def minNumber(self, nums1: List[int], nums2: List[int]) -> int:
         ans = 2**32
-        for i in nums1:
-            if i in nums2:
-                ans = min(ans,i)
+        nums1.sort()
+        nums2.sort()
+        p1,p2 = 0,0
+        while p1<len(nums1) and p2<len(nums2):
+            print("debug",p1,p2)
+            if nums1[p1] == nums2[p2]:
+                ans = min(ans,nums1[p1])
+                p1+=1
+                p2+=1
+
+            elif nums1[p1] < nums2[p2]:
+                p1+=1
+            else:
+                p2+=1
+
+        # for i in nums1:
+        #     if i in nums2:
+        #         ans = min(ans,i)
 
         mn1 = min(nums1)
         mn2 = min(nums2)
