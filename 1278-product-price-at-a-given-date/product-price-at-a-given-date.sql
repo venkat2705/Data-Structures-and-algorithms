69fb07a9dba0e73db245af6d739216ac12258233
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-13 23:21:00 -0800

    Time: 879 ms (54.53%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -3,15 +3,3 @@ WITH CTE AS (SELECT *, RANK() OVER(PARTITION BY product_id ORDER BY change_date 
 
 SELECT p.product_id, IFNULL(c.new_price,10) as price FROM CTE c RIGHT JOIN products p on p.product_id = c.product_id where c.rnk = 1 or c.rnk IS NULL GROUP BY p.product_id
 
-# with cte as (select * from products where change_date <= '2019-08-16'), 
-# cte2 as (select product_id,new_price, change_date, rank() over(partition by product_id order by change_date desc) as rn from cte)
-
-# select * from cte2
-
-# select p.product_id, ifnull(c.new_price , 10) as price
-# from cte2 c right join products p on p.product_id = c.product_id 
-# where c.rn = 1 or c.rn IS NULL group by p.product_id
- 
-
-
-
