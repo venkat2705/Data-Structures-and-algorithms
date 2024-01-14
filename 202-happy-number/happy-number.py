fe56b0d0cbe3d91132eb3487ea8e89693dc7ec0f
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-14 12:35:19 -0800

    Time: 33 ms (91.67%) | Memory: 17.2 MB (40.30%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        if n == 1:
+            return True
+        while True:
+            r = 0
+            res = 0
+            while n:
+                r = n%10
+                res += r*r
+                n //= 10
+            if res == 1:
+                return True
+            if res < 7:
+                return False
+            n = res
+            
+
+            
+
+
+
+        
