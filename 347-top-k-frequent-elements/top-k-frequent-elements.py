a9eb1fcf09436c13f48b59799642b146e52b9805
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-01-14 17:36:56 -0800

    Time: 82 ms (96.28%) | Memory: 21.9 MB (40.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -9,28 +9,10 @@ class Solution:
             c. iterate from the end of the buckets and add values to an array
             d. return array till k index
         '''
-        n = len(nums)
-        if n == k:
-            return nums
-
-        freq_map = {}
-        for val in nums:
-            freq_map[val] = freq_map.get(val, 0) + 1
-
-        # created empty buckets (freq: 1, n)
-        buckets = [[] for i in range(n+1)]
-
-        # iterate through frequency map and add values to the bucket
-        for key, val in freq_map.items():
-            buckets[val].append(key)
-        print(buckets)
-        flatten = []
-        for i in range(n, -1, -1):
-            for val in buckets[i]:
-                flatten.append(val)
-        
-        top_k_freq = []
-        for i in range(k):
-            top_k_freq.append(flatten[i])
-
-        return top_k_freq
+        d = Counter(nums)
+        d = sorted(d.items(), key = lambda items : items[1])
+        print(d)
+        ans = []
+        for i in d:
+            ans.append(i[0])
+        return ans[len(ans)-k:]
