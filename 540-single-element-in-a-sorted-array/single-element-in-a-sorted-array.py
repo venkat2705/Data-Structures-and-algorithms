df52539e380203f882772f2771aba653a7f61c9c
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-02-02 10:36:21 -0800

    Time: 143 ms (62.19%) | Memory: 26.7 MB (64.74%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -3,24 +3,20 @@ class Solution:
         l,h = 0,len(nums)-1
         while l<=h:
             m = (l+h)//2
-            # print(l,h,m)
-            if (m+1)%2 == 1:
-                # print(l,h,m)
-                if m<len(nums)-1 and  nums[m+1] == nums[m]:
-                    l=m+1
-                
-                elif m>0 and nums[m-1] == nums[m]: 
-                    h=m-1
+            if (m+1)%2:
+                if m < len(nums)-1 and nums[m] == nums[m+1]:
+                    l = m+1
+                elif m > 0 and nums[m] == nums[m-1]:
+                    h = m-1
                 else:
                     return nums[m]
-            
             else:
-                if m>0 and nums[m-1] == nums[m]:
-                    l=m+1
-                elif m<len(nums)-1 and nums[m+1] == nums[m]: 
-                    h=m-1
+                if m < len(nums)-1 and nums[m] == nums[m+1]:
+                    h = m-1
+                elif m > 0 and nums[m] == nums[m-1]:
+                    l = m+1
                 else:
                     return nums[m]
-            print(l,h,m)
+
 
         
