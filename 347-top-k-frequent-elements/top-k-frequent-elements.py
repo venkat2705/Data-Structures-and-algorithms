138b25adf8e9bd5b194d0ac7be5812db975bf797
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-02-06 15:14:38 -0800

    Time: 77 ms (98.25%) | Memory: 21.4 MB (67.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,10 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        '''
-        instead of sorting the dictionary in a reverse order, which takes O(NlogN). We can consider other approaches as well
-        1. using priority queue (comparision will be frequency of the element)--> O(Nlogk)
-        2. using bucket sort 
-            a. create frequency map
-            b. create buckets and add each element into frequency bucket 
-            c. iterate from the end of the buckets and add values to an array
-            d. return array till k index
-        '''
         d = Counter(nums)
-        d = sorted(d.items(), key = lambda items : items[1])
-        print(d)
-        ans = []
-        for i in d:
-            ans.append(i[0])
-        return ans[len(ans)-k:]
+        mn = []
+        for key,val in d.items():
+            heappush(mn,(val,key))
+            if len(mn) > k:
+                print("debug")
+                heappop(mn)
+        return [i[1] for i in mn]
