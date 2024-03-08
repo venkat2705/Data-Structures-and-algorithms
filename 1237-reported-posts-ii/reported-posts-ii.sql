47c019d2be045fac74ca3641a84c56e15da381e8
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-08 14:06:54 -0800

    Time: 3552 ms (5.12%) | Memory: 0B (100.00%) - LeetSync

--- 1237-reported-posts-ii/reported-posts-ii.sql
+++ 1237-reported-posts-ii/reported-posts-ii.sql
@@ -3,23 +3,3 @@
 with cte as (select count(distinct r.post_id)/count(distinct a.post_id) as cnt from actions a left join removals r on a.post_id = r.post_id where extra = 'spam' group by a.action_date)
 
 select round(avg(cnt)*100,2) as average_daily_percent from cte 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- with cte as (Select count(distinct r.post_id)/count(distinct a.post_id) as cnt from actions a left join removals r on a.post_id = r.post_id where extra = 'spam' group by a.action_date)
-
--- select round(avg(cnt)*100,2) as average_daily_percent from cte 
