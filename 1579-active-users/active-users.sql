b94078d1b4d59cb0615b653a6333f4a2ae31e5f2
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-08 11:39:19 -0800

    Time: 2816 ms (39.74%) | Memory: 0B (100.00%) - LeetSync

--- 1579-active-users/active-users.sql
+++ 1579-active-users/active-users.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
 
--- select a.name, l.*, row_number() over(partition by l.id) as rn from logins l join accounts a on l.id = a.id 
-SELECT DISTINCT l1.id,
-(select name from accounts where id = l1.id) name 
+select distinct l1.id, (select name from accounts where id = l1.id) as name 
 from logins l1 join logins l2 
 on l1.id = l2.id and datediff(l2.login_date, l1.login_date) between 1 and 4
-group by l1.id,l1.login_date
+group by l1.id, l1.login_date
 having count(distinct l2.login_date) = 4
-
 
 
 
