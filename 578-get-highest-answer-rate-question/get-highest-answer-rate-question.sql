9ca6e178cc7f55ac1ae19bad4fc2bcf45ec7b4bf
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-13 11:06:50 -0700

    Time: 595 ms (29.13%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,7 +1,23 @@
 # Write your MySQL query statement below
 
-with cte as (select question_id, sum(CASE WHEN action = 'answer' then 1 else 0 end) / sum(CASE WHEN action = 'show' then 1 else 0 end) as answer_cnt from surveylog group by question_id)
+WITH cte AS (SELECT question_id, 
+SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END) / SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END) as ratio FROM SurveyLog GROUP BY question_id)
 
-select question_id as survey_log from cte order by answer_cnt desc, question_id limit 1
+
+SELECT question_id as survey_log from cte ORDER BY ratio desc, question_id LIMIT 1 
+
+
+
+
+
+
+
+
+
+
+
+-- with cte as (select question_id, sum(CASE WHEN action = 'answer' then 1 else 0 end) / sum(CASE WHEN action = 'show' then 1 else 0 end) as answer_cnt from surveylog group by question_id)
+
+-- select question_id as survey_log from cte order by answer_cnt desc, question_id limit 1
 
 
