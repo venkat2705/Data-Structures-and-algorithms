ee4823bd5b14e1e5f8f662532f531ddfa9148d3e
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-19 23:42:05 -0700

    Time: 39 ms (59.84%) | Memory: 16.8 MB (21.94%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.py
+++ 249-group-shifted-strings/group-shifted-strings.py
@@ -1,15 +1,43 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        d={}
+        d = {}
         for word in strings:
             key = ()
             for i in range(len(word)-1):
-                circular_diff = 26 + ord(word[i+1]) - ord(word[i])
+                circular_diff = 26 + ord(word[i]) - ord(word[i+1])
                 key += (circular_diff%26,)
             if key in d:
                 d[key] += [word]
             else:
                 d[key] = []+[word]
-            # d[key] = d.get(key,[])+[word]
         print(d)
-        return list(d.values())
+        return d.values()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # d={}
+        # for word in strings:
+        #     key = ()
+        #     for i in range(len(word)-1):
+        #         circular_diff = 26 + ord(word[i+1]) - ord(word[i])
+        #         key += (circular_diff%26,)
+        #     if key in d:
+        #         d[key] += [word]
+        #     else:
+        #         d[key] = []+[word]
+        #     # d[key] = d.get(key,[])+[word]
+        # print(d)
+        # return list(d.values())
