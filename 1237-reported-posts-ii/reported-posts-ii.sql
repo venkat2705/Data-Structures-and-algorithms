3f265e7b63c487a060fd32ae948aac761beaf2fb
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-21 12:53:22 -0700

    Time: 2235 ms (17.06%) | Memory: 0B (100.00%) - LeetSync

--- 1237-reported-posts-ii/reported-posts-ii.sql
+++ 1237-reported-posts-ii/reported-posts-ii.sql
@@ -1,5 +1,20 @@
 # Write your MySQL query statement below
 
-with cte as (select count(distinct r.post_id)/count(distinct a.post_id) as cnt from actions a left join removals r on a.post_id = r.post_id where extra = 'spam' group by a.action_date)
+with cte as (select count(distinct r.post_id) / count(distinct a.post_id) as cnt from actions a left join removals r on a.post_id = r.post_id where a.extra = 'spam' group by a.action_date)
 
 select round(avg(cnt)*100,2) as average_daily_percent from cte 
+
+
+
+
+
+
+
+
+
+
+
+
+-- with cte as (select count(distinct r.post_id)/count(distinct a.post_id) as cnt from actions a left join removals r on a.post_id = r.post_id where extra = 'spam' group by a.action_date)
+
+-- select round(avg(cnt)*100,2) as average_daily_percent from cte 
