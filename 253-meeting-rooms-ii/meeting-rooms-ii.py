18b351d60f45ae223f3badafe1349d3a96de3d97
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-25 15:54:31 -0700

    Time: 82 ms (17.51%) | Memory: 20.2 MB (21.79%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,18 +1,55 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # we are counting number of meetings going on in parallel and the max of that will be the total number of rooms that we need
+
         start, end = [], []
         for i in intervals:
             start.append(i[0])
             end.append(i[1])
         start.sort()
         end.sort()
-        s_ptr, e_ptr, meetings_cnt, ans = 0, 0, 0, 0
-        while s_ptr < len(start) and e_ptr < len(end):
-            if start[s_ptr] < end[e_ptr]:
-                meetings_cnt += 1
-                s_ptr += 1
+        p1, p2, rooms_cnt, ans  = 0, 0, 0, 0
+        while p1 < len(start) and p2 < len(end):
+            if start[p1] < end[p2]:
+                rooms_cnt += 1
+                p1 += 1
             else:
-                meetings_cnt -= 1
-                e_ptr += 1
-            ans = max(ans,meetings_cnt)
+                rooms_cnt -= 1
+                p2 += 1
+            ans = max(ans, rooms_cnt)
         return ans
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # start, end = [], []
+        # for i in intervals:
+        #     start.append(i[0])
+        #     end.append(i[1])
+        # start.sort()
+        # end.sort()
+        # s_ptr, e_ptr, meetings_cnt, ans = 0, 0, 0, 0
+        # while s_ptr < len(start) and e_ptr < len(end):
+        #     if start[s_ptr] < end[e_ptr]:
+        #         meetings_cnt += 1
+        #         s_ptr += 1
+        #     else:
+        #         meetings_cnt -= 1
+        #         e_ptr += 1
+        #     ans = max(ans,meetings_cnt)
+        # return ans
