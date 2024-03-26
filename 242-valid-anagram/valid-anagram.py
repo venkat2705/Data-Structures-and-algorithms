51f638d7cbc1520a4cd468cec1fad4083d7e9996
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-26 14:48:02 -0700

    Time: 720 ms (5.03%) | Memory: 17 MB (64.99%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        Assign each prime number to the char position ex. a-2, b-3...z-101
+        Calculate the product --> as the prime porduct ensures the uniqueness
+        if product(s) == product(t) then a anagram
+        '''
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,101]
+        cnt1, cnt2 = 1, 1
+        for i in range(len(s)):
+            cnt1 = cnt1* primes[ord(s[i])-97]
+        for i in range(len(t)):
+            cnt2 = cnt2* primes[ord(t[i])-97]
+        return cnt1 == cnt2
+
+
+        
