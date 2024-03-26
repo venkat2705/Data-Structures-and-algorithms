4240de4e3b295e7335b1e32e6698d07a04069da1
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-26 14:48:32 -0700

    Time: 688 ms (5.03%) | Memory: 16.9 MB (64.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -7,9 +7,11 @@ class Solution:
         '''
         primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,101]
         cnt1, cnt2 = 1, 1
+        if len(s)!=len(t):
+            return False
         for i in range(len(s)):
             cnt1 = cnt1* primes[ord(s[i])-97]
-        for i in range(len(t)):
+        # for i in range(len(t)):
             cnt2 = cnt2* primes[ord(t[i])-97]
         return cnt1 == cnt2
 
