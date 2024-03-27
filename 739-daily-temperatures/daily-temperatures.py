88f990bea74aca62cfd1d816f62dda7931b0526b
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-03-26 22:25:16 -0700

    Time: 887 ms (67.94%) | Memory: 32 MB (18.90%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,11 +1,36 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n=len(temperatures)
-        st = []
-        ans = [0]*n
-        for ind,temp in enumerate(temperatures):
+        st, res = [], [0]*len(temperatures)
+        for ind, temp in enumerate(temperatures):
             while st and temp > st[-1][0]:
-                st_temp,st_ind = st.pop()
-                ans[st_ind] = ind - st_ind
-            st.append([temp,ind])
-        return ans
+                st_temp, st_ind = st.pop()
+                res[st_ind] = ind - st_ind
+            st.append([temp, ind])
+        return res
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # n=len(temperatures)
+        # st = []
+        # ans = [0]*n
+        # for ind,temp in enumerate(temperatures):
+        #     while st and temp > st[-1][0]:
+        #         st_temp,st_ind = st.pop()
+        #         ans[st_ind] = ind - st_ind
+        #     st.append([temp,ind])
+        # return ans
