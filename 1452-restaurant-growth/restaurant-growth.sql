78bb8deb7cf45f60095c8aaed21e13a0a741a0b1
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-04-27 16:32:21 -0700

    Time: 692 ms (60.62%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,50 +1,10 @@
 # Write your MySQL query statement below
 
--- with cte1 as (select customer_id, name, visited_on, sum(amount) as amount from customer group by visited_on order by visited_on),
+with cte1 as (select customer_id, name, visited_on, sum(amount) as amount from customer group by visited_on order by visited_on),
 
--- cte2 as (select visited_on, sum(amount) over(order by visited_on range between interval '6' day preceding and current row) as amount, row_number() over(order by visited_on) as rn from customer)
+cte2 as (select visited_on, sum(amount) over(order by visited_on range between interval '6' day preceding and current row) as amount, row_number() over(order by visited_on) as rn from cte1)
 
--- select distinct visited_on, amount, round(amount/7, 2) as average_amount from cte2 where rn > 6 order by visited_on
-
-
-
-with cte as (select customer_id, name, visited_on, sum(amount) as amount from customer group by visited_on order by visited_on),
-
-cte2 as (select visited_on, 
-sum(amount) over(ORDER BY visited_on RANGE BETWEEN INTERVAL '6' DAY PRECEDING AND CURRENT ROW) as amount, 
-row_number() over(order by visited_on) as rn from cte)
+select distinct visited_on, amount, round(amount/7, 2) as average_amount from cte2 where rn > 6 order by visited_on
 
 
-select distinct visited_on, amount, round(amount/7,2) as average_amount from cte2 where rn > 6 order by visited_on
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# with cte as (select customer_id,name,visited_on,sum(amount) as amount from customer where visited_on>=(select min(visited_on) as mn from customer)+6 group by visited_on),
-
-# cte2 as (select visited_on, sum(amount) as amt from customer where visited_on not in (select visited_on from cte))
-
-# select visited_on,
-# #(select amt from cte2) + amount, 
-# round(((select amt from cte2) + amount)/7,2) as average_amount
-# from cte
-# group by visited_on
-# order by visited_on
 
