8780c36cdcbedca6d053b84d1a20f641122e0c89
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-04-27 16:31:58 -0700

    Time: 616 ms (83.72%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,13 +1,21 @@
 # Write your MySQL query statement below
 
+-- with cte1 as (select customer_id, name, visited_on, sum(amount) as amount from customer group by visited_on order by visited_on),
+
+-- cte2 as (select visited_on, sum(amount) over(order by visited_on range between interval '6' day preceding and current row) as amount, row_number() over(order by visited_on) as rn from customer)
+
+-- select distinct visited_on, amount, round(amount/7, 2) as average_amount from cte2 where rn > 6 order by visited_on
+
+
+
 with cte as (select customer_id, name, visited_on, sum(amount) as amount from customer group by visited_on order by visited_on),
 
 cte2 as (select visited_on, 
 sum(amount) over(ORDER BY visited_on RANGE BETWEEN INTERVAL '6' DAY PRECEDING AND CURRENT ROW) as amount, 
-row_number() over(order by visited_on) as rn from cte )
+row_number() over(order by visited_on) as rn from cte)
 
 
-select visited_on, amount, round(amount/7,2) as average_amount from cte2 where rn > 6 order by visited_on  
+select distinct visited_on, amount, round(amount/7,2) as average_amount from cte2 where rn > 6 order by visited_on
 
 
 
