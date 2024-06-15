8557ebef4628e2340193e4a79639d5653eda4762
Venkata Ramana <60357150+venkat2705@users.noreply.github.com>
2024-06-15 15:00:09 -0700

    Time: 91 ms (44.32%) | Memory: 21.1 MB (68.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
-        for i in nums:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
-        d1 = sorted(d.items(), key = lambda x : x[1])
-        print(d1)
-        ans = []
-        for i in d1:
-            ans.append(i[0])
-        return ans[len(ans)-k:]
+        d= Counter(nums)
+        heap = []
+        for key, val in d.items():
+            heappush(heap, (val, key))
+            if len(heap) > k:
+                heappop(heap)
+        return [i[1] for i in heap]
+
+
+
+
+
+
+
+
+
+
+
+
+        # d = {}
+        # for i in nums:
+        #     if i in d:
+        #         d[i] += 1
+        #     else:
+        #         d[i] = 1
+        # mn = []
+        # for key, val in d.items():
+        #     heappush(mn, (val,key))
+        #     if len(mn)>k:
+        #         heappop(mn)
+        # return [i[1] for i in mn]
 
 
 
